e39195910487229c6b36671aa978e29850bdfed6
Hand-of-Cthulhu <necrorerxx@gmail.com>
2016-12-16 20:27:17 +1300

    Added The Virus Code

---
+++ Source/TankTryCpp/Private/TMapExt.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTryCpp.h"
+#include "TMapExt.h"
+
+TMapExt::TMapExt()
+{
+}
+
+TMapExt::~TMapExt()
+{
+}
