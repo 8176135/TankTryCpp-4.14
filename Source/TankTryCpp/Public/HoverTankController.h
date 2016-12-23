3145171f2a5e14f6e89c0815791171297c306fba
Hand-of-Cthulhu <necrorerxx@gmail.com>
2016-12-23 15:38:58 +1300

    Not Crashing

---
+++ Source/TankTryCpp/Public/HoverTankController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "HoverTankController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKTRYCPP_API AHoverTankController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
